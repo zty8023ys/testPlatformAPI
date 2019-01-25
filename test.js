d6a4a502a6a1a54b394eff8361568b4b2f63fe39
周天洋 <zhoutianyang@sanshengshi.co>
2019-01-25 17:32:25 +0800

    1

---
+++ test.js
@@ -0,0 +1,19 @@
+const url = "https://serene-keller-2d034d.netlify.com"
+const onlineMap = {}
+const addToOnlineMap = (gid, version = "") => {
+    if (typeof (onlineMap[gid]) !== undefined) {
+        console.error("gid:", gid, "重复配置!");
+        debugger;
+    }
+    onlineMap[gid] = version;
+}
+addToOnlineMap(9);
+addToOnlineMap(1, 1);
+addToOnlineMap(14, 2);
+window.getPlatformAPIUrl = (gid) => {
+    const version = onlineMap[gid];
+    if (!version) {
+        return url + "/PlatformAPI_browserify.min.js";
+    }
+    return url + "/PlatformAPI_browserify" + version + ".min.js";
+}
